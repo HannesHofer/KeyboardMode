2912cfe3f46b6ea2690a12d2ae3859887c4962b4
Hannes Hofer <Hannes.Hofer@gmail.com>
2014-07-12 01:43:25 +0200

    options javascript workig
    javascript for selecting, storing and show options is now working.
    also vimium css --- not neccessary stuff was removed.
    options are stored in chrome store now, but read out for actual usage is missing.
    maybe we can use load functions in options.js

--- options.js
+++ options.js
@@ -9,17 +9,21 @@ function deactivateMetakey(deactivatetype, caller) {
       }
     }
   }
+  saveAll();
 }
 
-function deactivateDefault(def) {
+function deactivateDefault(def, nosave) {
   var names = ['follow', 'newtab', 'newbacktab', 'incognito', 'backincognito'];
   var types = ['strg', 'alt', 'shift', 'deactivated']
   /*********** deactivate impossible options ************/
   for (name in names) {
     for (type in types) {
       id = document.getElementById(names[name] + types[type])
-      if (id != null)
+      if (id != null) {
+	if (id.checked && (def === names[name]))
+	  id.checked = false;
 	id.disabled = (def === names[name]);
+      }
     }
     
     var text = document.getElementById(names[name])
@@ -29,8 +33,11 @@ function deactivateDefault(def) {
     text = document.getElementById(names[name]+'text')
     if (text != null)
       text.style.color = (def === names[name]) ? 'gray' : 'black';
+    
   }
   /*********** deactivate impossible options ***********/ 
+  if (!nosave)
+    saveAll();
 }
 
 function storeKeycombination() {
@@ -50,12 +57,26 @@ function storeLinkHintDefault() {
 }
 
 function storeLinkMetaKey() {
+  var follow, newtab, newbacktab, incognito, backincognito;
+  follow = document.querySelector('input[name = "defaultopen"]:checked');
+  newtab = document.querySelector('input[name = "defaultnewtab"]:checked');
+  newbacktab = document.querySelector('input[name = "defaultnewbacktab"]:checked');
+  incognito = document.querySelector('input[name = "defaultincognito"]:checked');
+  backincognito = document.querySelector('input[name = "defaultbackincognito"]:checked');
+  
+  
+  follow = (follow === null) ? "" : follow.id;
+  newtab = (newtab === null) ? "" : newtab.id;
+  newbacktab = (newbacktab === null) ? "" : newbacktab.id;
+  incognito = (incognito === null) ? "" : incognito.id;
+  backincognito = (backincognito === null) ? "" : backincognito.id;
+  
   chrome.storage.sync.set({
-    metakeyfollow: document.querySelector('input[name = "follow"]:checked').id,
-    metakeynewtab: document.querySelector('input[name = "newtab"]:checked').id,
-    metakeynewbacktab: document.querySelector('input[name = "newbacktab"]:checked').id,
-    metakeyincognito: document.querySelector('input[name = "incognito"]:checked').id,
-    metakeybackincognito: document.querySelector('input[name = "backincognito"]:checked').id
+    metakeyfollow: follow,
+    metakeynewtab: newtab,
+    metakeynewbacktab: newbacktab,
+    metakeyincognito: incognito,
+    metakeybackincognito: backincognito
   }, null);
 }
 
@@ -89,9 +110,22 @@ function loadAll() {
   
   loadLinkHintDefault(function(items) {
     document.getElementById(items.linkhintdefault).checked = true;
-    deactivateDefault(items.linkhintdefault)
+    deactivateDefault(items.linkhintdefault, true)
   });
   
+  loadLinkMetaKey(function(items) {
+    if (items.metakeyfollow != "")
+      document.getElementById(items.metakeyfollow).checked = true;
+    if (items.metakeynewtab != "")
+      document.getElementById(items.metakeynewtab).checked = true;
+    if (items.metakeynewbacktab != "")
+      document.getElementById(items.metakeynewbacktab).checked = true;
+    if (items.metakeyincognito != "")
+      document.getElementById(items.metakeyincognito).checked = true;
+    if (items.metakeybackincognito != "")
+      document.getElementById(items.metakeybackincognito).checked = true;
+    
+  });
   
 }
 
@@ -117,4 +151,46 @@ function loadKeycombination(func) {
   }, func);
 }
 
+function loadLinkMetaKey(func) {
+  chrome.storage.sync.get({
+    metakeyfollow: "",
+    metakeynewtab: "",
+    metakeynewbacktab: "",
+    metakeyincognito: "",
+    metakeybackincognito: ""
+  }, func);  
+}
+/********************* eventhandlers for onclick ******************/
+
+window.onload = function() {
+  document.getElementById('follow').onclick = function(){ deactivateDefault('follow',false);}
+  document.getElementById('newtab').onclick = function(){deactivateDefault('newtab',false);}
+  document.getElementById('newbacktab').onclick = function(){ deactivateDefault('newbacktab',false);}
+  document.getElementById('incognito').onclick = function(){ deactivateDefault('incognito',false);}
+  document.getElementById('backincognito').onclick = function(){ deactivateDefault('backincognito',false);}
+
+  document.getElementById('followstrg').onclick = function(){ deactivateMetakey('follow','strg');}
+  document.getElementById('followalt').onclick = function(){ deactivateMetakey('follow','alt');}
+  document.getElementById('followshift').onclick = function(){ deactivateMetakey('follow','shift');}
+  document.getElementById('newtabstrg').onclick = function(){ deactivateMetakey('newtab','strg');}
+  document.getElementById('newtabalt').onclick = function(){ deactivateMetakey('newtab','alt');}
+  document.getElementById('newtabshift').onclick = function(){ deactivateMetakey('newtab','shift');}
+  document.getElementById('newbacktabstrg').onclick = function(){ deactivateMetakey('newbacktab','strg');}
+  document.getElementById('newbacktabalt').onclick = function(){ deactivateMetakey('newbacktab','alt');}
+  document.getElementById('newbacktabshift').onclick = function(){ deactivateMetakey('newbacktab','shift');}
+  document.getElementById('incognitostrg').onclick = function(){ deactivateMetakey('incognito','strg');}
+  document.getElementById('incognitoalt').onclick = function(){ deactivateMetakey('incognito','alt');}
+  document.getElementById('incognitoshift').onclick = function(){ deactivateMetakey('incognito','shift');}
+  document.getElementById('backincognitostrg').onclick = function(){ deactivateMetakey('backincognito','strg');}
+  document.getElementById('backincognitoalt').onclick = function(){ deactivateMetakey('backincognito','alt');}
+  document.getElementById('backincognitoshift').onclick = function(){ deactivateMetakey('backincognito','shift');}
+  
+  document.getElementById('hintchars').oninput = function(){ storeHintChars();}
+  document.getElementById('keycombKeycode').oninput = function(){ storeKeycombination();}
+  document.getElementById('keycombStrg').onclick = function(){ storeKeycombination();}
+  document.getElementById('keycombAlt').onclick = function(){ storeKeycombination();}
+  document.getElementById('keycombShift').onclick = function(){ storeKeycombination();}    
+}
+
 document.addEventListener('DOMContentLoaded', loadAll);
+/********************* eventhandlers for onclick ******************/
